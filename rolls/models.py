b790fb298e8e8ac890ac00de5712e94d1482e543
Iffahan Suksuwan <jackcopsluncher@gmail.com>
2022-02-24 18:29:10 +0700

    add Roll, chane to OneToOneField

--- rolls/models.py
+++ rolls/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,unique=True)
     url = models.URLField("Website", blank=True)
     company = models.CharField(max_length=50, blank=True)
