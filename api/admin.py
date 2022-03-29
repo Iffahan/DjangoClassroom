939df3b3f25878b04734dcd186f9c1bf33f54e4b
Iffahan Suksuwan <jackcopsluncher@gmail.com>
2022-03-29 22:13:49 +0700

    update pip freeze

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Classroom)
-# admin.site.register(Student)
 admin.site.register(Assignment)
 admin.site.register(AssignmentStatus)
 admin.site.register(Message)
--- main/settings.py
+++ main/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'rest_framework_simplejwt',
 
     'regist.apps.RegistConfig',
     'api.apps.ApiConfig',
